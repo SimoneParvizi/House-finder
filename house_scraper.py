de50eef1b20b26b35cb6a6345f7f94f6b7b20dff
Simone <simone.parvizi@outlook.it>
2023-10-12 22:51:00 +0200

    Update house_scraper.py

--- house_scraper.py
+++ house_scraper.py
@@ -5,6 +5,18 @@ import time
 import smtplib
 from twilio.rest import Client
 import logging
+import random
+#%%
+
+##############################################################################
+#############                                                 ################
+#                                                                            #   
+# Make sure to handle the privacy and terms of service of each website you   #
+# scrape, as web scraping may be prohibited or restricted by some websites   # 
+# Always read and respect the website's robots.txt file and terms of service #
+#                                                                            #
+#############                                                 ################
+##############################################################################
 
 # Set Chrome options for headless browsing
 chrome_options = Options()
@@ -13,60 +25,95 @@ logging.basicConfig(level=logging.INFO)
 
 driver = webdriver.Chrome(options=chrome_options)
 
-def check_new_listings(previous_listings):
-    driver.get('https://www.huurstunt.nl/studio/huren/amsterdam/+2km/0-1500/')
+WEBSITES = [
+    {
+        'url': 'https://www.huurstunt.nl/studio/huren/amsterdam/+2km/0-1500/',
+        'listing_selector': '.listing-item',
+        'address_selector': '.address-class',  # Example, update with correct selector
+        'email_selector': '.email-class'      # Example, update with correct selector
+    },
+    
+    {
+        'url': 'https://another-website.com/some-page/',
+        'listing_selector': '.another-listing-item',
+        'address_selector': '.address-class-for-another-website',  # This needs to be updated
+        'email_selector': '.email-class-for-another-website'      # This needs to be updated
+    }
+
+]
+
+def check_new_listings(url, listing_selector):
+    driver.get(url)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    current_listings = soup.select('.listing-item')
-    
-    new_listings = [listing for listing in current_listings if listing not in previous_listings]
-    
-    return new_listings, current_listings
+    current_listings = soup.select(listing_selector)
+    return current_listings
 
+def send_notification_to_owner(listing, address_selector, email_selector, url):
+    address = listing.select_one(address_selector).text
+    owner_email = listing.select_one(email_selector).text
 
-def send_notification(new_listings):
-    logging.info(f'New listings found: {new_listings}')
-    # Twilio Credentials
-    account_sid = 'your_account_sid'
-    auth_token = 'your_auth_token'
-    client = Client(account_sid, auth_token)
+    # Customize the message based on the house address
+    email_message_to_owner = f"Subject: Interested in your listing at {address}\n\nI am interested in the house at {address}. Please contact me for further details."
 
-    whatsapp_message = f"New listings found: {new_listings}"
-    message = client.messages.create(
-                              from_='whatsapp:your_twilio_whatsapp_number',
-                              body=whatsapp_message,
-                              to='whatsapp:your_personal_whatsapp_number'
-                          )
+    # Send email to owner
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()  # Upgrade the connection to secure encrypted SSL/TLS connection
+        server.login("youremail@gmail.com", "your_password_or_app_specific_password")
 
-    print(f'Message sent with ID: {message.sid}')
-
-
-    # Email via SMTP
-    with smtplib.SMTP('smtp.example.com', 587) as server:
-        server.login("simone.parvizi@outlook.it", "your_email_password")
-        email_message = f"Subject: New Listings Found\n\n{whatsapp_message}"
+        # Send email to the owner
         server.sendmail(
-            "simone.parvizi@outlook.it",
-            "simone.parvizi@outlook.it",
-            email_message
+            "youremail@gmail.com",
+            owner_email,
+            email_message_to_owner
         )
 
+        # Send a notification email to yourself with the URL of the house
+        notification_email = f"Subject: Sent an email about {address}\n\nYou sent an email expressing interest in the house at {address}. Here's the URL for reference: {url}"
+        
+        # Sending the notification to yourself by replying to the email you sent to the owner
+        server.sendmail(
+            "youremail@gmail.com",
+            "youremail@gmail.com",
+            notification_email,
+            headers={"In-Reply-To": owner_email, "References": owner_email}  # These headers make the email appear as a reply in email clients
+        )
 
-previous_listings = []
+previous_listings = {}
+MAX_PREVIOUS_LISTINGS = 100 
 
 try:
-    previous_listings = []
     while True:
-        new_listings, previous_listings = check_new_listings(previous_listings)
-        if new_listings:
-            # Send notification
-            send_notification(new_listings)
-        else:
-            logging.info(' No new listings found.')  
-        time.sleep(5)  # Wait for 5 seconds before refreshing the page
+        for website in WEBSITES:
+            url = website['url']
+            listing_selector = website['listing_selector']
+            address_selector = website['address_selector']
+            email_selector = website['email_selector']
 
+            logging.info(f'Checking new listings for {url}')
+            
+            try:
+                current_listings = check_new_listings(url, listing_selector)
+            except Exception as e:
+                logging.error(f'Error checking listings for {url}: {e}')
+                continue  # Skip to the next website
+            
+            if url not in previous_listings:
+                previous_listings[url] = []
+            
+            new_listings = [listing for listing in current_listings if listing not in previous_listings[url]]
+            
+            for listing in new_listings:
+                try:
+                    send_notification_to_owner(listing, address_selector, email_selector, url)
+                    logging.info(f'Sent notification for new listing at {url}')
+                except Exception as e:
+                    logging.error(f'Error sending notification for listing at {url}: {e}')
+
+            previous_listings[url] = current_listings[:MAX_PREVIOUS_LISTINGS]
+        
+        sleep_duration = random.randint(40, 80)
+        logging.info(f'Sleeping for {sleep_duration} seconds')
+        time.sleep(sleep_duration)
 
 except Exception as e:
-    logging.error(f'An error occurred: {e}')
-
-finally:
-    driver.quit()  # This will close the browser and quit the driver
+    logging.error(f'An unexpected error occurred: {e}')
